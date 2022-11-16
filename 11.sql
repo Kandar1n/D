776d0b9d4fad202915d292a4bd101a129eec292f
Daniil <vosemsemodin@gmail.com>
2022-11-16 22:53:43 +0300

    12

--- 11.sql
+++
@@ -1,8 +0,0 @@
-use sakila;
-#A union B: {L,M,N,O,P,Q,R,S,T}
-#A union all B: {L,M,N,O,P,P,Q,R,S,T}
-#A intersect B: {P}
-#A except B: {L,M,N,O}
-select "Actor" typ, a.first_name, a.last_name from actor a where last_name like 'L%' 
-union all
-select "Customer" typ, c.first_name, c.last_name from customer c where last_name like 'L%' order by last_name;#6.2 и 6.3
