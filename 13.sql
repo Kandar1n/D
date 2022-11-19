1aa2b781e070e0457636af35c8f0e95a6d678f1f
Daniil <vosemsemodin@gmail.com>
2022-11-20 01:39:53 +0300

    14

--- 13.sql
+++
@@ -1,7 +0,0 @@
-use sakila;
-select count(*) from payment;
-select customer_id, count(*), sum(amount) from payment group by customer_id ;
-select customer_id, count(*), sum(amount) from payment group by customer_id having count(*)>40;
-use bd;
-select count(*) from bd.users u;
-select id_genre, count(*) from bd.movies m group by id_genre having count(*)>3;
